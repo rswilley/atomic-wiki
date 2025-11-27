2c6b5222117dad5fed4e718662d94cae0a2773b1
Robert Swilley <robert@rswilley.com>
2025-11-27 11:34:40 -0600

    Lucene index store setup

---
+++ Wiki/Services/ServiceService.cs
@@ -0,0 +1,27 @@
+﻿namespace Wiki.Services;
+
+public interface ISearchService
+{
+    IEnumerable<SearchSnippet> Search(string searchQuery);
+}
+
+public class SearchService(ISearchStore searchStore) : ISearchService
+{
+    public IEnumerable<SearchSnippet> Search(string searchQuery)
+    {
+        var result = searchStore.Search(searchQuery);
+        if (result?.Snippets?.Any() == true)
+        {
+            var searchResults = result.Snippets.Select(r => new SearchSnippet
+            {
+                PermanentId = r.PermanentId,
+                Title = r.Title,
+                Snippet = r.Snippet
+                //Snippet = _noteService.RemoveTitle(_formatService.ToHtml(r.Snippet))
+            });
+            return searchResults;
+        }
+
+        return [];
+    }
+}
