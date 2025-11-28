336d0c3d6b012f7fa3cdf17f63da2bd92325e40a
Robert Swilley <robert@rswilley.com>
2025-11-28 09:55:25 -0600

    save page to search index

--- Wiki/Services/PageService.cs
+++ Wiki/Services/PageService.cs
@@ -9,25 +9,37 @@ public interface IPageService
 
 public class PageService(
     IPageStore pageStore,
+    ISearchStore searchStore,
     IIdService idService,
     IMarkdownService markdownService) : IPageService
 {
     public async Task<string> Save(PageWriteModel page)
     {
         var permanentId = idService.Generate(DateTime.UtcNow.Ticks);
+        var title = markdownService.GetFirstHeader(page.Markdown) ?? "Untitled";
+        var tags = GetTags(page.Tags);
+        
         await pageStore.Save(new PageDocument
         {
             Meta = new PageFrontMatter
             {
                 PermanentId = permanentId,
-                Title = markdownService.GetFirstHeader(page.Markdown) ?? "Untitled",
+                Title = title,
                 Type = page.Type,
                 Category = page.Category,
-                Tags = GetTags(page.Tags),
+                Tags = tags,
                 Pinned = page.IsPinned,
                 CreatedAt = page.CreatedAt
             },
             Markdown = page.Markdown
+        });
+        
+        searchStore.Create(new PageSearchItem
+        {
+            PermanentId = permanentId,
+            Title = title,
+            Body = page.Markdown,
+            Tags = tags ?? []
         });
         return permanentId;
     }
